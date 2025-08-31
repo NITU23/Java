da057e6059dd0f202b22a70c390e8d77a9e1f08a
nitin.vyas <nitinvyas200@gmail.com>
2025-08-31 19:12:02 +0530

    Servlet added

---
+++ DemoApp/src/main/java/com/addServlet/AddServlet.java
@@ -0,0 +1,65 @@
+package com.addServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class AddServlet extends HttpServlet {
+
+	private void processRequest(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+
+		int k = i + j;
+
+		System.out.println("Result is : " + k);
+		// the below code is for normal request.
+		/*
+		 * res.setContentType("text/html"); try (PrintWriter out = res.getWriter()) {
+		 * out.println("<h1>Result is : " + k + "</h1>"); }
+		 */
+
+		// this code is for calling another servlet
+		// the is the one way of calling the servlet.
+		/*req.setAttribute("k", k);
+		RequestDispatcher rd = req.getRequestDispatcher("/sq");
+		rd.forward(req, res); */
+		
+		//  calling the servlet using url redirecting.
+		/*res.sendRedirect("sq?k="+k); */
+		
+		// sending values using session
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		res.sendRedirect("sq");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		System.out.println("I am in gett");
+		processRequest(req, res);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		System.out.println("I am in post");
+		processRequest(req, res);
+
+	}
+
+//	@Override // the service method is treating the each request same, no difference in get or
+//				// post
+//	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		try {
+//			processRequest(req, res);
+//		} catch (IOException | ServletException e) {
+//			e.printStackTrace();
+//		}
+//	}
+}
---
+++ DemoApp/src/main/java/com/addServlet/SqServlet.java
@@ -0,0 +1,39 @@
+package com.addServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class SqServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		// normal servlet calling
+		int k = (int) req.getAttribute("k");
+
+		k = k * k;
+		PrintWriter out = res.getWriter();
+		out.println("Hello I am sq servlet" + k);
+
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		// send Redirect only uses the get method
+
+		/*
+		 * int k = Integer.parseInt(req.getParameter("k")); k = k*k;
+		 */
+
+		// value fetching using session
+		HttpSession session = req.getSession();
+		int k = (int)session.getAttribute("k");
+		PrintWriter out = res.getWriter();
+		out.println("Hello I am sq get servlet" + k);
+
+	}
+}
